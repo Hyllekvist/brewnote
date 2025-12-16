aaf5983295bc54d54bf4d6b946abccdd7186fc26
Hyllekvist <42335892+Hyllekvist@users.noreply.github.com>
2025-12-16 21:25:58 +0100

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
+
 export function supabaseServer() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-  return createClient(url, anon);
+  const service =
+    process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+  return createClient(url, service, {
+    auth: { persistSession: false },
+  });
 }
